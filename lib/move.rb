43be13ac361b2c0b868a2dcbc4fc00fff5917709
Hugo <huwgoma@gmail.com>
2022-03-27 19:41:05 -0400

    Write Move::stack - returns Move class' @@stack variable, containing all Move objects in sequential order

--- lib/move.rb
+++ lib/move.rb
@@ -1,10 +1,17 @@
 # frozen_string_literal: true
 
 class Move
+  @@stack = []
+
   def initialize(start_cell, end_cell, piece, killed_piece)
     @start = start_cell
     @end = end_cell
     @piece = piece
     @killed = killed_piece
+    @@stack << self
+  end
+
+  def self.stack
+    @@stack
   end
 end
--- spec/move_spec.rb
+++ spec/move_spec.rb
@@ -1,2 +1,13 @@
 # frozen_string_literal: true
 require './lib/move'
+
+describe Move do
+  describe '::stack' do
+    # Contains an array of Move objects - First in, Last out
+    it "returns Move's @@stack class variable" do
+      move_1 = described_class.new('d2', 'd3', 'WPawn', nil)
+      move_2 = described_class.new('d7', 'd3', 'BRook', 'WPawn')
+      expect(Move.stack).to eq([move_1, move_2])
+    end
+  end
+end
