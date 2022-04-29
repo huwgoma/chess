fb91ec90fa1fa925c54d8746c08d7c0e32bd32f8
Hugo <huwgoma@gmail.com>
2022-04-28 21:57:21 -0400

    Write skeleton for program flow in main.rb - what happens after the game_mode_message is printed?

--- lib/main.rb
+++ lib/main.rb
@@ -19,6 +19,9 @@ loop do
   
   puts tutorial_message
 # puts game_mode_message
+# select_game_mode
+# if game mode = 1, start a new game and play it
+# else if game mode = 2, load selected game (select later) and play that
   game = Game.new
   game.play
 
